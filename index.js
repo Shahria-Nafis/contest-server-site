276cb938182e9250c3fb4b77f46dd2155be7f64c
Shahria Nafis <shahrianafis1@gmail.com>
2026-01-12 20:23:23 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'https://client-nine-blue-35.vercel.app',
-    'https://contest-hub-navy.vercel.app'
+    'https://contest-server-site.vercel.app'
   ],
   credentials: true
 }));
